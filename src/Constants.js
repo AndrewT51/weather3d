8a00aac4eafeaabc3daf13bbed4163f5d489f1fe
Andrew Trigg <andrewt51@hotmail.com>
2016-09-29 00:46:12 +0100

    API key stored as env vriable

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,5 @@
 var Constants = {
-  API_KEY: 'dca680da44d3f5a3',
+  API_KEY: process.env.WUNDERGROUND_API,
   urls:{
     autoIp: function autoIp(){
       return 'http://api.wunderground.com/api/'+ Constants.API_KEY + '/geolookup/q/autoip.json'
