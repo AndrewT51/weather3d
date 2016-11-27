60ebbf789df1c05d05244e2ba4169f7ba726ae3d
Andrew Trigg <andrewt51@hotmail.com>
2016-11-27 23:12:00 +0000

    Changed http to https

--- dist/bundle.js
+++ dist/bundle.js
@@ -20405,16 +20405,16 @@
 	  API_KEY: 'dca680da44d3f5a3',
 	  urls: {
 	    autoIp: function autoIp() {
-	      return 'http://api.wunderground.com/api/' + Constants.API_KEY + '/geolookup/q/autoip.json';
+	      return 'https://api.wunderground.com/api/' + Constants.API_KEY + '/geolookup/q/autoip.json';
 	    },
 	    autoComplete: function autoComplete(city, country) {
-	      return 'http://autocomplete.wunderground.com/aq?query=' + city + (country ? '&c=' + country : '') + '&format=JSON';
+	      return 'https://autocomplete.wunderground.com/aq?query=' + city + (country ? '&c=' + country : '') + '&format=JSON';
 	    },
 	    geoLookup: function geoLookup(latLong) {
-	      return 'http://api.wunderground.com/api/' + Constants.API_KEY + '/geolookup/q/' + latLong + '.json';
+	      return 'https://api.wunderground.com/api/' + Constants.API_KEY + '/geolookup/q/' + latLong + '.json';
 	    },
 	    forecast10day: function forecast10day(location) {
-	      return 'http://api.wunderground.com/api/' + Constants.API_KEY + '/forecast10day/q/' + location + '.json';
+	      return 'https://api.wunderground.com/api/' + Constants.API_KEY + '/forecast10day/q/' + location + '.json';
 	    }
 	  }
 	};
